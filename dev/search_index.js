eaa1dc99c1605500d25e27b6edcc090a83baf327
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 13:03:25 +0000

    build based on 7fc5168

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TensorNQueens","page":"Home","title":"TensorNQueens","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorNQueens.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
