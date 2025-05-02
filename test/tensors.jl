a48264c152b36edab2137748e79e15630f106662
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-05-02 13:50:40 +0800

    fox tests

--- test/tensors.jl
+++ test/tensors.jl
@@ -4,7 +4,7 @@ using TensorNQueens: generate_tensor, TensorNQ, generate_tensor_network, generat
 using OMEinsum
 
 @testset "generate_tensor" begin
-	tensor, t10, t01, t11 = generate_tensor(Float64)
+	tensor = generate_tensor(Float64)
 	@test tensor[2, 1, 1, 1, 1, 1, 1, 1, 1] == 0.0
 	@test tensor[2, 1, 1, 1, 1, 1, 1, 1, 2] == 1.0
 	@test tensor[1, 1, 1, 1, 1, 1, 1, 1, 2] == 0.0
@@ -14,30 +14,22 @@ using OMEinsum
 	@test tensor[1, 1, 1, 1, 2, 1, 2, 2, 2] == 0.0
 	@test tensor[1, 2, 1, 1, 2, 2, 2, 2, 2] == 0.0
     @test tensor[1, 1, 1, 1, 1, 2, 1, 1, 1] == 0.0
-	@test t10 == [1, 0]
-	@test t01 == [0, 1]
-	@test t11 == [1, 1]
-end
-
-@testset "TensorNQ" begin
-	t = TensorNQ((1, 2, 3, 4, 5, 6, 7, 8, 9))
-	@show t
 end
 
 @testset "generate_TensorNQ_lattice" begin
-	tn, pos11, pos01 = generate_TensorNQ_lattice(3)
-	@test tn[2, 2].labels[3] == tn[1, 1].labels[7]
-	@test tn[2, 2].labels[4] == tn[1, 2].labels[6]
-	@test tn[2, 2].labels[8] == tn[1, 3].labels[2]
-	@test tn[2, 2].labels[1] == tn[2, 1].labels[9]
-	@test tn[2, 2].labels[9] == tn[2, 3].labels[1]
-	@test tn[2, 2].labels[2] == tn[3, 1].labels[8]
+	tn_lattice = generate_TensorNQ_lattice(3)
+	@test tn_lattice.lattice[2, 2].labels[3] == tn_lattice.lattice[1, 1].labels[7]
+	@test tn_lattice.lattice[2, 2].labels[4] == tn_lattice.lattice[1, 2].labels[6]
+	@test tn_lattice.lattice[2, 2].labels[8] == tn_lattice.lattice[1, 3].labels[2]
+	@test tn_lattice.lattice[2, 2].labels[1] == tn_lattice.lattice[2, 1].labels[9]
+	@test tn_lattice.lattice[2, 2].labels[9] == tn_lattice.lattice[2, 3].labels[1]
+	@test tn_lattice.lattice[2, 2].labels[2] == tn_lattice.lattice[3, 1].labels[8]
 
-	@test tn[3, 1].labels[6] ∈ pos01
-	@test tn[2, 2].labels[5] ∈ pos11
-	@test tn[1, 3].labels[9] ∈ pos01
-	@test tn[3, 3].labels[9] ∈ pos01
-	@test tn[3, 3].labels[6] ∈ pos01
+	@test tn_lattice.lattice[3, 1].labels[6] ∈ tn_lattice.pos01
+	@test tn_lattice.lattice[2, 2].labels[5] ∈ tn_lattice.pos11
+	@test tn_lattice.lattice[1, 3].labels[9] ∈ tn_lattice.pos01
+	@test tn_lattice.lattice[3, 3].labels[9] ∈ tn_lattice.pos01
+	@test tn_lattice.lattice[3, 3].labels[6] ∈ tn_lattice.pos01
 end
 
 @testset "generate_tensor_network" begin
